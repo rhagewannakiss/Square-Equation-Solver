86dcd0698c87fcf08b69e7a290ab6cabc114ebf8
rhage <139259164+rhagewannakiss@users.noreply.github.com>
2024-08-26 15:56:19 +0300

    ну тут что-то в функции вынесено и ввод буковковый

--- final_ss_solver.cpp
+++ final_ss_solver.cpp
@@ -5,7 +5,6 @@
 
 
 
-
 typedef struct SS_Tests
 {
     int    num_Test;
@@ -17,12 +16,15 @@ typedef struct SS_Tests
     int n_roots_exp;
 } SS_Tests;
 
+int SS_Solver_Func(double a, double b, double c, double* x1, double* x2);
 int Quadric_Func(double a, double b, double c, double* x1, double* x2);
 int Linear_Func(double b, double c, double* x1, double* x2);
+int  sollution(double x1, double x2, int n_roots);
+void Input_Func(double* a, double* b, double* c);
+int compare_roots(double x1, double x1_exp);
 int Double_Diff_comp(double a, double b);
-int SS_Solver_Func(double a, double b, double c, double* x1, double* x2);
-int compare_roots(double x1, double x1_exp); /*!*/
 void func_Tester(SS_Tests data);
+void Test_func();
 
 //--------Enum-for-roots-cases-----------------------------------------------------------------------------
 
@@ -38,63 +40,20 @@ typedef enum Roots_case
 
 int main (void)
 {
-
-struct SS_Tests mass[11] = {{.num_Test = 0,.a = 0, .b = 5, .c = 8, .x1_exp = -1.6, .x2_exp = NAN, .n_roots_exp = 1},
-                             {.num_Test = 1,.a = 0, .b = 0, .c = 0, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = -1},
-                             {.num_Test = 2,.a = 0, .b = 0, .c = 8, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = 0},
-                             {.num_Test = 3,.a = 0, .b = 5, .c = 0, .x1_exp = 0, .x2_exp = NAN, .n_roots_exp = 1},
-                             {.num_Test = 4,.a = 1, .b = 0, .c = 0, .x1_exp = 0, .x2_exp = NAN, .n_roots_exp = 1},
-                             {.num_Test = 5,.a = 7, .b = 0, .c = 8, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = 0},
-                             {.num_Test = 6,.a = 4, .b = 0, .c = -8, .x1_exp = sqrt(2), .x2_exp = -sqrt(2), .n_roots_exp = 2},
-                             {.num_Test = 7,.a = 1, .b = -12, .c = 36, .x1_exp = 6, .x2_exp = NAN, .n_roots_exp = 1},
-                             {.num_Test = 8,.a = -224, .b = 5, .c = -8, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = 0},
-                             {.num_Test = 9, .a = -486, .b = 5, .c = -8, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = 0},
-                             {.num_Test = 10, .a = 1, .b = -4, .c = 5, .x1_exp = NAN, .x2_exp = NAN, .n_roots_exp = 0}
-                            };
-
-
-    int nTests = (sizeof(mass)/sizeof(mass[0]));
-
-    for(int i=0; i < nTests; i++)
-    {
-        func_Tester(mass[i]);
-    }
-
-
-    printf ("# Enter coefficients of a square equation a, b, c:   \n");
-    printf ("# Please use real numbers instead of letters, words etc.  \n");
+    Test_func();
 
     double a = 0;
     double b = 0;
     double c = 0;
 
-    while (scanf ("%lg %lg %lg", &a, &b, &c) == 3)
-        {
-        double   x1 = 0;
-        double   x2 = 0;
-        int n_roots = 0;
+    Input_Func(&a, &b, &c);
 
-        n_roots = SS_Solver_Func(a, b, c, &x1, &x2);
+    double   x1 = 0;
+    double   x2 = 0;
+    int n_roots = SS_Solver_Func(a, b, c, &x1, &x2);
 
-        switch (n_roots)
-        {
-            case ZERO_Roots: printf ("No roots found.\n");
-            break;
-
-            case   ONE_Root: printf ("Given Square Equation has only 1 (one) root: x = %lg\n", x1);
-            break;
-
-            case  TWO_Roots: printf ("Given Square Equation has 2 (two) roots: x1 = %lg, x2 = %lg\n", x1, x2);
-            break;
-
-            case  INF_Roots: printf ("Given Square Equation has Infinite number of roots.");
-            break;
-
-            default:         printf ("main(): ERROR: n_roots = %d\n", n_roots);
-            break;
-        }
-    }
-   }
+    sollution(x1, x2, n_roots);
+}
 
 //---Introduction-of-a-constant-and-a-function-comparing-the-difference-of-double-numbers--------------
 
@@ -109,17 +68,16 @@ int Double_Diff_comp(double a, double b)
 }
 
 //----------------Square-Equation-Solving-Function-----------------------------------------------------
-
 /*linear equation (a==0)*/
 
 int Linear_Func(double b, double c, double* x1, double* x2)
 {
-    assert(x1 != NULL);
-    assert(x2 != NULL);
+    assert(x1 !=  NULL);
+    assert(x2 !=  NULL);
     assert(isfinite(b));
     assert(isfinite(c));
 
-    if(Double_Diff_comp(b,0))
+    if (Double_Diff_comp(b,0))
     {
         if (Double_Diff_comp(c,0))
         {
@@ -154,8 +112,8 @@ int Linear_Func(double b, double c, double* x1, double* x2)
 
 int Quadric_Func(double a, double b, double c, double* x1, double* x2)
 {
-    assert(x1 != NULL);
-    assert(x2 != NULL);
+    assert(x1 !=  NULL);
+    assert(x2 !=  NULL);
     assert(isfinite(a));
     assert(isfinite(b));
     assert(isfinite(c));
@@ -181,7 +139,6 @@ int Quadric_Func(double a, double b, double c, double* x1, double* x2)
             *x2 = NAN;
             return ZERO_Roots;
         }
-
     }
  }
 
@@ -203,7 +160,7 @@ int SS_Solver_Func(double a, double b, double c, double* x1, double* x2)
     return n_roots;
 }
 
-//-------Function-for-Testing------------------------------------------------------------------------------
+//-------Function-for-Testing--------------------------------------int  readable_coef(double x1, double x2, int n_roots)----------------------------------------
 
 void func_Tester(SS_Tests data)
 {
@@ -231,6 +188,8 @@ void func_Tester(SS_Tests data)
     }
 }
 
+//----------------------------------------------------------------------------------------------------------
+
 int compare_roots(double x1, double x1_exp)
 {
     if ((Double_Diff_comp(x1, x1_exp))||(isnan(x1) && isnan(x1_exp)))
@@ -240,3 +199,65 @@ int compare_roots(double x1, double x1_exp)
     else return 0;
 }
 
+//-----------------------------------------------------------------------------------------------------------
+
+int  sollution(double x1, double x2, int n_roots)
+{
+    switch (n_roots)
+    {
+        case ZERO_Roots: return printf ("No roots found.\n");
+        break;
+
+        case   ONE_Root: return printf ("Given Square Equation has only 1 (one) root: x = %lg\n", x1);
+        break;
+
+        case  TWO_Roots: return printf ("Given Square Equation has 2 (two) roots: x1 = %lg, x2 = %lg\n", x1, x2);
+        break;
+
+        case  INF_Roots: return printf ("Given Square Equation has Infinite number of roots.");
+        break;
+
+        default:         return printf ("main(): ERROR: n_roots = %d\n", n_roots);
+        break;
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------------
+
+void Test_func()
+{
+struct SS_Tests mass[] =    {{.num_Test = 0,   .a =    0,  .b =   5,  .c =  8,  .x1_exp =    -1.6,  .x2_exp =      NAN,    .n_roots_exp =  1},
+                             {.num_Test =  1,  .a =    0,  .b =   0,  .c =  0,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp = -1},
+                             {.num_Test =  2,  .a =    0,  .b =   0,  .c =  8,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp =  0},
+                             {.num_Test =  3,  .a =    0,  .b =   5,  .c =  0,  .x1_exp =       0,  .x2_exp =      NAN,    .n_roots_exp =  1},
+                             {.num_Test =  4,  .a =    1,  .b =   0,  .c =  0,  .x1_exp =       0,  .x2_exp =      NAN,    .n_roots_exp =  1},
+                             {.num_Test =  5,  .a =    7,  .b =   0,  .c =  8,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp =  0},
+                             {.num_Test =  6,  .a =    4,  .b =   0,  .c = -8,  .x1_exp = sqrt(2),  .x2_exp = -sqrt(2),    .n_roots_exp =  2},
+                             {.num_Test =  7,  .a =    1,  .b = -12,  .c = 36,  .x1_exp =       6,  .x2_exp =      NAN,    .n_roots_exp =  1},
+                             {.num_Test =  8,  .a = -224,  .b =   5,  .c = -8,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp =  0},
+                             {.num_Test =  9,  .a = -486,  .b =   5,  .c = -8,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp =  0},
+                             {.num_Test = 10,  .a =    1,  .b =  -4,  .c =  5,  .x1_exp =     NAN,  .x2_exp =      NAN,    .n_roots_exp =  0}
+                            };
+
+    int nTests = (sizeof(mass)/sizeof(mass[0]));
+
+    for(int i=0; i < nTests; i++)
+    {
+        func_Tester(mass[i]);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------
+
+void Input_Func(double* a, double* b, double* c)
+{
+    printf ("# Enter coefficients of a square equation a, b, c:   \n");
+    printf ("# Please use real numbers instead of letters, words etc.  \n");
+    printf ("# If you would love the program to stop running, please enter 'Thanks!' \n");
+
+    while (scanf("%lg %lg %lg", a, b, c) != 3)
+    {
+        while (getchar() != '\n') {}
+        printf ("#Invalid input! Please, enter numbers only! =(  \n");
+    }
+}
